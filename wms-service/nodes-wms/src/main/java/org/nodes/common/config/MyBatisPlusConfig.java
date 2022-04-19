f6d059544fcb341b40f316850d585f94965d2826
邓茂林 <dengml@nodes.com.cn>
2022-04-19 13:33:49 +0800

    feat:增加mybatis-plus的分页插件配置

---
+++ wms-service/nodes-wms/src/main/java/org/nodes/common/config/MyBatisPlusConfig.java
@@ -0,0 +1,29 @@
+package org.nodes.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"org.springblade", "org.nodes"})
+@MapperScan({"org.springblade.**.mapper.**", "org.nodes.**.mapper.**"})
+public class MyBatisPlusConfig {
+
+	/**
+	 * MybatisPlus插件Bean
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(
+			// 新的分页插件,一缓和二缓遵循mybatis的规则
+			new PaginationInnerInterceptor(DbType.MYSQL)
+		);
+		return interceptor;
+	}
+
+}
