6429d34142fdc9fa369a39651604ab2dd1e51e22
pengyongcheng <pyc_1215@163.com>
2022-04-25 11:56:15 +0800

    feat:Asn单管理模块优化

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/instock/asn/mapper/AsnDetailMapper.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/instock/asn/mapper/AsnDetailMapper.java
@@ -4,10 +4,18 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.nodes.wms.dao.instock.asn.entities.AsnDetail;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * 收货单明细表 Mapper 接口
  */
 @Repository("asnDetailRepository")
 public interface AsnDetailMapper extends BaseMapper<AsnDetail> {
-
+	/**
+	 * 根据Asn单id查询Asn单明细id
+	 *
+	 * @param asnBillIdList:
+	 * @return java.util.List<java.lang.Long>
+	 */
+	List<Long> selectAsnDetailId(List<Long> asnBillIdList);
 }
