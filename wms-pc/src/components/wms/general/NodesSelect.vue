f413ef4af3d0c6e5c380ef6e2b7bc29c73fe5da1
pengyongcheng <pyc_1215@163.com>
2022-05-10 17:26:18 +0800

    feat:下拉组件底层模板value显示在前、name显示在后，在props中新增注释

--- wms-pc/src/components/wms/general/NodesSelect.vue
+++ wms-pc/src/components/wms/general/NodesSelect.vue
@@ -2,7 +2,7 @@
     <el-select
         v-model="val"
         :multiple="multiple"
-        collapse-tags
+        collapse-tags="false"
         placeholder="请选择"
         size="mini"
         style="width:100%;"
@@ -13,10 +13,8 @@
             :key="item.value"
             :label="item[labelName]"
             :value="item">
-
-                <span style="float: left" v-if="isCustomTemplate">{{ item[labelName] }}</span>
-                <span style="float: right; color: #8492a6; font-size: 13px" v-if="isCustomTemplate">{{ item[valueName] }}</span>
-
+            <span style="float: right" v-if="isCustomTemplate">{{ item[labelName] }}</span>
+            <span style="float: left; color: #8492a6; font-size: 13px" v-if="isCustomTemplate">{{ item[valueName] }}</span>
         </el-option>
     </el-select>
 </template>
@@ -32,12 +30,19 @@ export default {
         event: 'selectValChange'
     },
     props: {
-        selectVal: [Number, Array, String],
+        // 选中的数据
+        selectVal: [Number, Array, String, Object],
+        // 是否多选 false:单选 默认为多选
         multiple: {type: Boolean, required: false, default: true},
+        // 数据源
         dataSource: {type: Array, required: true},
+        // 唯一标识
         valueKey:{type: String, required: false,default:()=>'value'},
+        // label属性名
         labelName:{type: String, required: false,default:()=>'label'},
+        // value属性名
         valueName:{type: String, required: false,default:()=>'value'},
+        // value和name是否都显示，true:都显示 默认为不都显示
         isCustomTemplate:{type: Boolean, required: false,default:()=>false},
     },
     data() {
