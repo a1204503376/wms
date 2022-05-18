2c1a5e02a036f9186d934d7cad09cae0baa62eea
penghao <1204503376@qq.com>
2022-05-18 19:36:34 +0800

    feat: 新增供应商和物料下拉框接收对象

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/basics/sku/dto/SkuSelectRequest.java
@@ -0,0 +1,27 @@
+package org.nodes.wms.dao.basics.sku.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 物品下拉选择 返回对象
+ */
+@Data
+public class SkuSelectRequest implements Serializable {
+	private static final long serialVersionUID = 21290733663648044L;
+	/**
+	 * 物品主键ID
+	 */
+	private Long skuId;
+
+	/**
+	 * 物品编码
+	 */
+	private String skuCode;
+
+	/**
+	 * 物品名称
+	 */
+	private String skuName;
+}
