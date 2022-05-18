b2ca2291ab02a9b0a924abebadbece6c6021f30e
Liumh <1062646441@qq.com>
2022-05-18 11:58:12 +0800

    build:update vue.config.js

--- wms-pc/vue.config.js
+++ wms-pc/vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
     css: {
         extract: {
             ignoreOrder: true,
-            extract: true
+            extract: false
         }
     },
     //开发模式反向代理配置，生产模式请使用Nginx部署并配置反向代理
