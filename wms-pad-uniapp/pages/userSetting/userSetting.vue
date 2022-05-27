ed5ff5cbb7f4d1d5c1af172d07a915c11fe3c2b3
penghao <1204503376@qq.com>
2022-05-27 10:51:55 +0800

    feat: 手持前端修改密码优化

--- wms-pad-uniapp/pages/userSetting/userSetting.vue
+++ wms-pad-uniapp/pages/userSetting/userSetting.vue
@@ -1,5 +1,4 @@
 <template>
-
 	<view>
 		<view class="user-box">
 			<view class="user">
@@ -7,27 +6,18 @@
 			</view>
 			<view class="user-name" format="yyyy/MM/dd" style=" font-weight: bolder">用户名：{{ username }}
 				<br /><br />登录时间:{{loginTime}}</view>
-
 		</view>
-
-
-		<view class="cell" style="margin-left: 10%;margin-right: 10%;">
-
-			<div>
+		<view  style="margin-left: 10%;margin-right: 10%;">
 				<br /><br />
 				<u--form>
-					<u-form-item borderBottom labelWidth="60" @click="submit">
+					<u-form-item borderBottom labelWidth="60" @click="toPwdSetting">
 						<u-icon name="edit-pen" size="28"></u-icon>
 						<text style="font-size: 100%;">修改密码</text>
 					</u-form-item>
-				</u--form>
-
-
-			</div>
+				</u--form>		
 		</view>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
@@ -36,17 +26,8 @@
 				loginTime: uni.getStorageSync('loginTime')
 			}
 		},
-		onLoad() {
-			uni.$u.func.registerScanner(this.scannerCallback);
-		},
-		onUnload() {
-			uni.$u.func.unRegisterScanner();
-		},
 		methods: {
-			scannerCallback(data) {
-				this.address = data
-			},
-			submit() {
+			toPwdSetting() {
 				uni.$u.func.route('/pages/userSetting/pwdSetting');
 			}
 		}
