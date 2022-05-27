8a725322411cd947caad6447dab89f4a31ef7148
penghao <1204503376@qq.com>
2022-05-27 09:04:42 +0800

    feat: 手持前端修改密码优化

--- wms-pad-uniapp/pages/userSetting/pwdSetting.vue
+++ wms-pad-uniapp/pages/userSetting/pwdSetting.vue
@@ -2,24 +2,27 @@
 	<view>
 		<!-- <u-navbar  @leftClick="navigateBack" : :fixed="false" :autoBack="false" title="修改密码" >
 		    </u-navbar> -->
-	<view style="margin-top: 5%;margin-left:5%;margin-right: 5%;">
-		<u--form labelPosition="left" :model="form" :rules="rules" ref="uForm">
-			<u-form-item prop="userInfo.oldPassword" borderBottom ref="item1">
-				<u--input v-model="form.userInfo.oldPassword" :focus="oldPwdFocus" border="none" placeholder="请输入原始密码" @confirm="oldPwd"></u--input>
-			</u-form-item>
-			<u-form-item prop="userInfo.newPassword" borderBottom ref="item1">
-				<u--input v-model="form.userInfo.newPassword" :focus="newPwdFocus" border="none" placeholder="请输入新密码" @confirm="newPwd" ></u--input>
-			</u-form-item>
-			<u-form-item prop="userInfo.newPassword1" borderBottom ref="item1">
-				<u--input v-model="form.userInfo.newPassword1" border="none" :focus="newPwd1Focus" placeholder="请输入确认密码" @confirm="submit"></u--input>
-			</u-form-item>
+		<view style="margin-top: 5%;margin-left:5%;margin-right: 5%;">
+			<u--form labelPosition="left" :model="form" :rules="rules" ref="uForm">
+				<u-form-item prop="userInfo.oldPassword" borderBottom ref="item1">
+					<u--input v-model="form.userInfo.oldPassword" :focus="oldPwdFocus" border="none"
+						placeholder="请输入原始密码" @confirm="oldPwd"></u--input>
+				</u-form-item>
+				<u-form-item prop="userInfo.newPassword" borderBottom ref="item1">
+					<u--input v-model="form.userInfo.newPassword" :focus="newPwdFocus" border="none"
+						placeholder="请输入新密码" @confirm="newPwd"></u--input>
+				</u-form-item>
+				<u-form-item prop="userInfo.newPassword1" borderBottom ref="item1">
+					<u--input v-model="form.userInfo.newPassword1" border="none" :focus="newPwd1Focus"
+						placeholder="请输入确认密码" @confirm="submit"></u--input>
+				</u-form-item>
 
-		</u--form>
-	</view>
-		<view >
-		
+			</u--form>
+		</view>
+		<view>
+
 			<u-button class="bt-1" type="primary" @click="navigateBack" text="返回[Esc]"></u-button>
-			<u-button class="bt-2" type="primary"  @click="submit" text="确认[Ent]"></u-button>
+			<u-button class="bt-2" type="primary" @click="submit" text="确认[Ent]"></u-button>
 		</view>
 
 	</view>
@@ -30,9 +33,9 @@
 	export default {
 		data() {
 			return {
-				oldPwdFocus:false,
-				newPwdFocus:false,
-				newPwd1Focus:false,
+				oldPwdFocus: false,
+				newPwdFocus: false,
+				newPwd1Focus: false,
 				form: {
 					userInfo: {
 						id: uni.getStorageSync('userInfo').user_id,
@@ -66,54 +69,38 @@
 			};
 		},
 		methods: {
-			oldPwd(){
-			uni.hideKeyboard();	//隐藏软键盘				
-			this.newPwdFocus = true
+			oldPwd() {
+				uni.hideKeyboard(); //隐藏软键盘				
+				this.newPwdFocus = true
 			},
-			newPwd(){
-				uni.hideKeyboard();	//隐藏软键盘
+			newPwd() {
+				uni.hideKeyboard(); //隐藏软键盘
 				this.newPwdFocus = false
 				this.newPwd1Focus = true
 			},
-				navigateBack(){
-							uni.navigateBack({
-								delta:1,//返回层数，2则上上页
-							})
-						},
+			navigateBack() {
+				uni.navigateBack({
+					delta: 1, //返回层数，2则上上页
+				})
+			},
 			submit() {
-                 this.newPwd1Focus = false;
+				this.newPwd1Focus = false;
 				this.$refs.uForm.validate().then(res => {
 					api.updatePassword(
 						this.form.userInfo.id,
 						md5(this.form.userInfo.oldPassword),
 						md5(this.form.userInfo.newPassword),
 						md5(this.form.userInfo.newPassword1)).then(data => {
-							
-					this.$u.func.showToast({title: '修改密码成功，请重新登录',duration:3000})
-						 this.$u.func.logout()
 
-						
+						this.navigateBack();
+
+
 					})
 				})
 			}
 		},
 	};
 </script>
-<style>
-	.bt-1 {
-		width: 50%;
-		position: fixed;
-		bottom: 0;
-		background-color: #33FFFF;
-
-	}
-
-	.bt-2 {
-		width: 50%;
-		position: fixed;
-		bottom: 0;
-		margin-left: 49%;
-		background-color: #00BBFF;
-
-	}
+<style lang="scss">
+	@import 'pwdSetting.scss';
 </style>
