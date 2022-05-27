2a7fe0ef39e4f257c44cd8430d7d88422ebcb1d8
pengyongcheng <pyc_1215@163.com>
2022-05-27 14:17:00 +0800

    feat:手持端新增签到页面

---
+++ wms-pad-uniapp/pages/userSetting/signSetting.vue
@@ -0,0 +1,122 @@
+<template>
+	<view class="parentClass">
+		<br /><br />
+		<view class="dateClass"> {{date}} </view>
+		<view>
+			<view class="bigRound">
+				<view class="smallRound" @click="onClick">
+					<span>{{type}}</span>
+				</view>
+			</view>
+		</view>
+		<view>
+			<p>签到状态</p>
+			<view id="line"></view>
+		</view>
+		<!-- <view class="u-page">
+			<u-list @scrolltolower="scrolltolower" >
+				<u-list-item v-for="(item, index) in list" :key="index">
+					<u-cell :title="`item.name`">
+						<u-avatar slot="icon" shape="square" size="35" :src="item.url"
+							customStyle="margin: -3px 5px -3px 0"></u-avatar>
+					</u-cell>
+				</u-list-item>
+			</u-list>
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				date: new Date(),
+				type: "签到",
+				list: [
+					{
+						icon: "我是头像",
+						name: "小王",
+						createTime: '2022-05-26 17:24:33',
+						type: "签到"
+					},
+					{
+						icon: "我是头像",
+						name: "小王",
+						createTime: '2022-05-26 17:30:33',
+						type: "签退"
+					},
+					{
+						icon: "我是头像",
+						name: "小王",
+						createTime: '2022-05-26 17:24:33',
+						type: "签到"
+					},
+					{
+						icon: "我是头像",
+						name: "小王",
+						createTime: '20442-05-26 17:30:33',
+						type: "签退"
+					},
+					{
+						icon: "我是头像",
+						name: "小王",
+						createTime: '2033-05-26 17:24:33',
+						type: "签到"
+					},
+					{
+						icon: "我是头像",
+						name: "小王",
+						createTime: '2010-05-26 17:30:33',
+						type: "签退"
+					},
+				]
+			}
+		},
+		methods: {
+			getListData() {
+				this.list = null;
+			},
+			onClick() {
+				this.type = this.type === "签到" ? "签退" : "签到"
+			},
+			// 列表触底触发的事件
+			scrolltolower(){
+				
+			},
+			// 时间格式化
+			dateFormat() {
+				var date = new Date()
+				var year = date.getFullYear()
+
+				// 在日期格式中，月份是从0开始的
+				// 使用三元表达式在小于10的前面加0，以达到格式统一  如 09:11:05
+
+				var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+				var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+				var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+				var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+				var seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+				let week = date.getDay() // 星期
+				let weekArr = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+				// 拼接 时间格式处理
+				return year + '年' + month + '月' + day + '日 ' + hours + '时' + minutes + '分' + seconds + '秒'
+			}
+		},
+		// 挂载时间
+		mounted() {
+			this.timer = setInterval(() => {
+				this.date = this.dateFormat()
+			})
+		},
+		// 销毁时清除计时器
+		beforeDestroy() {
+			if (this.timer) {
+				clearInterval(this.timer)
+			}
+		}
+	}
+</script>
+	
+<style lang="scss">
+	@import 'signSetting.scss';
+</style>
--- wms-pad-uniapp/pages/userSetting/userSetting.vue
+++ wms-pad-uniapp/pages/userSetting/userSetting.vue
@@ -5,16 +5,23 @@
 				<img src="/static/images/login.png" />
 			</view>
 			<view class="user-name" format="yyyy/MM/dd" style=" font-weight: bolder">用户名：{{ username }}
-				<br /><br />登录时间:{{loginTime}}</view>
+				<br /><br />登录时间:{{loginTime}}
+			</view>
 		</view>
-		<view  style="margin-left: 10%;margin-right: 10%;">
-				<br /><br />
-				<u--form>
-					<u-form-item borderBottom labelWidth="60" @click="toPwdSetting">
-						<u-icon name="edit-pen" size="28"></u-icon>
-						<text style="font-size: 100%;">修改密码</text>
-					</u-form-item>
-				</u--form>		
+		<view style="margin-left: 10%;margin-right: 10%;">
+			<br /><br />
+			<u--form>
+				<u-form-item borderBottom labelWidth="60" @click="toPwdSetting">
+					<u-icon name="edit-pen" size="28"></u-icon>
+					<text style="font-size: 100%;">修改密码</text>
+				</u-form-item>
+			</u--form>
+			<u--form>
+				<u-form-item borderBottom labelWidth="60" @click="sign">
+					<u-icon name="calendar-fill" size="28"></u-icon>
+					<text style="font-size: 100%;">签到</text>
+				</u-form-item>
+			</u--form>
 		</view>
 	</view>
 </template>
@@ -29,6 +36,9 @@
 		methods: {
 			toPwdSetting() {
 				uni.$u.func.route('/pages/userSetting/pwdSetting');
+			},
+			sign() {
+				uni.$u.func.route('/pages/userSetting/signSetting');
 			}
 		}
 	}
