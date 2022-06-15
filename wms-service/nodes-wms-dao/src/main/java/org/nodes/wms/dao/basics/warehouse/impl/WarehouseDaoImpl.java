fd244262502cb369aa6853c8ee49e14a3d462917
wangzy <NOdes2022>
2022-06-15 11:06:22 +0800

    feat：新增手持库房切换功能。手持设备在登录之后记录当前操作的库房,并以日志的形式进行存储

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/basics/warehouse/impl/WarehouseDaoImpl.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/basics/warehouse/impl/WarehouseDaoImpl.java
@@ -1,12 +1,14 @@
 package org.nodes.wms.dao.basics.warehouse.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.RequiredArgsConstructor;
 import org.nodes.wms.dao.basics.warehouse.WarehouseDao;
 import org.nodes.wms.dao.basics.warehouse.dto.output.WarehouseResponse;
 import org.nodes.wms.dao.basics.warehouse.entities.Warehouse;
 import org.nodes.wms.dao.basics.warehouse.mapper.WarehouseMapper;
 import org.springblade.core.mp.base.BaseServiceImpl;
+import org.springblade.core.tool.utils.Func;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -33,4 +35,25 @@ public class WarehouseDaoImpl extends BaseServiceImpl<WarehouseMapper, Warehouse
     public Warehouse findByCode(String whCode) {
 		return super.getOne(new LambdaQueryWrapper<Warehouse>().eq(Warehouse::getWhCode,whCode));
     }
+
+	@Override
+	public List<Warehouse> list() {
+		return super.list();
+	}
+
+	/**
+	 * 根据机构ID集合查询库房集合
+	 * @param deptIds 机构ID集合
+	 * @return 库房集合
+	 */
+	@Override
+	public List<Warehouse> getListByDeptId(List<Long> deptIds) {
+		if(Func.isEmpty(deptIds))
+		{
+			return null;
+		}
+		LambdaQueryWrapper<Warehouse> lambdaQueryWrapper= Wrappers.lambdaQuery(Warehouse.class);
+		lambdaQueryWrapper.in(Warehouse::getDeptId,deptIds);
+		return super.list(lambdaQueryWrapper);
+	}
 }
