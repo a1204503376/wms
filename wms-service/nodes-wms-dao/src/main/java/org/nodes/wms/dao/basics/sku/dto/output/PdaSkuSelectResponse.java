fa8034e9bc2932158860c5e505677fb1b51696e9
wangzy <NOdes2022>
2022-06-22 09:40:53 +0800

    feat：Sku的规格型号查询以及后端实现，

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/basics/sku/dto/output/PdaSkuSelectResponse.java
@@ -0,0 +1,30 @@
+package org.nodes.wms.dao.basics.sku.dto.output;
+
+
+
+import java.io.Serializable;
+
+/**
+ * 规格型号下拉框返回对象
+ **/
+public class PdaSkuSelectResponse implements Serializable {
+
+	private static final long serialVersionUID = -6365939402967791634L;
+	/**
+	 * 物品规格型号
+	 */
+	private String skuSpec;
+	private Long id=0L;
+
+	public String getLabel() {
+		return skuSpec;
+	}
+
+	public Long getId() {
+		return id++;
+	}
+
+	public void setSkuSpec(String skuSpec) {
+		this.skuSpec = skuSpec;
+	}
+}
