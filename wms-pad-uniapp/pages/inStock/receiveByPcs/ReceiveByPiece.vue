e42e0ea54b23b975425e7e46fbc66a3526a6fc18
wangzy <NOdes2022>
2022-06-23 15:23:33 +0800

    feat：PDA按件收货-收货页面-样式更改

--- wms-pad-uniapp/pages/inStock/receiveByPcs/ReceiveByPiece.vue
+++ wms-pad-uniapp/pages/inStock/receiveByPcs/ReceiveByPiece.vue
@@ -1,31 +1,30 @@
 <template>
 	<view>
-		<!-- 注意，如果需要兼容微信小程序，最好通过setRules方法设置rules规则 -->
-		<u--form labelPosition="left" :model="params">
-			<u-form-item label="物品" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.skuCode"></u--input>
+		<u--form>
+			<u-form-item label="物品:"  borderBottom class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.skuCode" :disabled="true"></input>
 			</u-form-item>
-			<u-form-item label="名称" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.skuName"></u--input>
+			<u-form-item label="名称:" borderBottom  class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.skuName" :disabled="true"></input>
 			</u-form-item>
-			<u-form-item label="型号" borderBottom class="textAlignClass" labelWidth="140rpx">
+			<u-form-item label="型号:" borderBottom class="textAlignClass" labelWidth="180rpx">
 				<uni-select v-model="params.skuLot2"></uni-select>
 			</u-form-item>
-			<u-form-item label="数量" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.surplusQty"></u--input>
+			<u-form-item label="数量:" borderBottom :required="true"  class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.surplusQty"></input>
 				<!-- <u-number-box v-model="params.skuCode" @change="valChange"></u-number-box> -->
 			</u-form-item>
-			<u-form-item label="UOM" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.umName" :disabled="true"></u--input>
+			<u-form-item label="UOM:" borderBottom  class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.umName" :disabled="true"></input>
 			</u-form-item>
-			<u-form-item label="生产批次" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.skuLot1"></u--input>
+			<u-form-item label="生产批次:" borderBottom :required="true"  class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.skuLot1"></input>
 			</u-form-item>
-			<u-form-item label="箱码" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.boxCode"></u--input>
+			<u-form-item label="箱码:" borderBottom class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.boxCode"></input>
 			</u-form-item>
-			<u-form-item label="LOC" borderBottom class="textAlignClass" labelWidth="140rpx">
-				<u--input v-model="params.locCode"></u--input>
+			<u-form-item label="LOC:" borderBottom :required="true" class="textAlignClass" labelWidth="180rpx">
+				<input v-model="params.locCode"></input>
 			</u-form-item>
 		</u--form>
 		<keyboard-listener @keydown="emitKeyDown"></keyboard-listener>
@@ -52,14 +51,14 @@
 		data() {
 			return {
 				params: {
-					skuCode:undefined,
-					skuName:undefined,
-					skuLot2:undefined,
-					surplusQty:undefined,
-					umName:undefined,
-					skuLot1:undefined,
-					boxCode:undefined,
-					locCode:'STAGE'
+					skuCode: undefined,
+					skuName: undefined,
+					skuLot2: undefined,
+					surplusQty: undefined,
+					umName: undefined,
+					skuLot1: undefined,
+					boxCode: undefined,
+					locCode: 'STAGE',
 				},
 				receiveDetailId: '',
 				receiveDetailList: [],
@@ -78,12 +77,12 @@
 		},
 		methods: {
 			submit() {
-				this.params.locCode=uni.getStorageSync('warehouse').whCode+this.params.locCode;
-				if(this.params.isSn==1){
+				this.params.locCode = uni.getStorageSync('warehouse').whCode + this.params.locCode;
+				if (this.params.isSn == 1) {
 					uni.$u.func.route('/pages/inStock/receiveByPcs/receiptDetailEnquiry', this.params);
-				    return;
+					return;
 				}
-				console.log(this.params.locCode)
+				console.log(this.params)
 				//提交表单数据 收货
 			},
 			getDetailByDetailId() {
@@ -111,5 +110,5 @@
 </script>
 
 <style>
-	
+
 </style>
