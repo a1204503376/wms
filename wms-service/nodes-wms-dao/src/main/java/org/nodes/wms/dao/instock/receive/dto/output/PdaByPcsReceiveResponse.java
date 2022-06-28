23e316ef48da1b71f381c4698d0cff634c169b3c
wangzy <NOdes2022>
2022-06-28 18:26:22 +0800

    feat：收货响应对象PdaByPieceReceiveResponse更名

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/instock/receive/dto/output/PdaByPcsReceiveResponse.java
@@ -0,0 +1,18 @@
+package org.nodes.wms.dao.instock.receive.dto.output;
+
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * 收货响应对象
+ **/
+@Data
+public class PdaByPcsReceiveResponse implements Serializable {
+	//当前收货是否完成
+	private String currentReceivieIsAccomplish;
+	//全部收货是否完成
+	private String allReceivieIsAccomplish;
+}
