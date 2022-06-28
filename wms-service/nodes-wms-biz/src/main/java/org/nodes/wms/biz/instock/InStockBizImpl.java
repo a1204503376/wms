04add905bf75e185078f76242a8f6f2546f0d5eb
wangzy <NOdes2022>
2022-06-28 15:54:35 +0800

    feat：优化按件收货

--- wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/instock/InStockBizImpl.java
+++ wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/instock/InStockBizImpl.java
@@ -3,6 +3,7 @@ package org.nodes.wms.biz.instock;
 import lombok.RequiredArgsConstructor;
 import org.nodes.wms.biz.instock.receive.ReceiveBiz;
 import org.nodes.wms.biz.instock.receive.modular.ReceiveFactory;
+import org.nodes.wms.biz.instock.receiveLog.ReceiveLogBiz;
 import org.nodes.wms.biz.stock.StockBiz;
 import org.nodes.wms.dao.instock.receive.dto.input.PdaByPieceReceiveRequest;
 import org.nodes.wms.dao.instock.receive.dto.input.ReceiveDetailLpnPdaRequest;
@@ -12,6 +13,7 @@ import org.nodes.wms.dao.instock.receive.dto.output.ReceiveDetailLpnPdaResponse;
 import org.nodes.wms.dao.instock.receive.entities.ReceiveDetail;
 import org.nodes.wms.dao.instock.receive.entities.ReceiveDetailLpn;
 import org.nodes.wms.dao.instock.receive.entities.ReceiveHeader;
+import org.nodes.wms.dao.instock.receiveLog.entities.ReceiveLog;
 import org.springblade.core.secure.utils.AuthUtil;
 import org.springblade.core.tool.utils.Func;
 import org.springframework.stereotype.Service;
@@ -25,45 +27,46 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class InStockBizImpl implements InStockBiz {
-    private  final ReceiveBiz receiveBiz;
+	private final ReceiveBiz receiveBiz;
 	private final ReceiveFactory receiveFactory;
 	private final StockBiz stockBiz;
+	private final ReceiveLogBiz receiveLogBiz;
 
 	@Transactional(propagation = Propagation.NESTED, rollbackFor = Exception.class)
 	@Override
 	public void receiveByBoxCode(ReceiveDetailLpnPdaRequest request) {
 		boolean hasReceiveHeaderId = Func.isNotEmpty(request.getReceiveHeaderId());
 		// 判断业务参数（无单收货除外），是否可以正常收货、超收
-        if(hasReceiveHeaderId){
-          for(ReceiveDetailLpnItemDto item: request.getReceiveDetailLpnItemDtoList()){
-			  receiveBiz.canReceive(item.getReceiveDetailId(),item.getPlanQty());
-		  }
-		}else{
+		if (hasReceiveHeaderId) {
+			for (ReceiveDetailLpnItemDto item : request.getReceiveDetailLpnItemDtoList()) {
+				receiveBiz.canReceive(item.getReceiveDetailId(), item.getPlanQty());
+			}
+		} else {
 			// 是否无单收货：判断当前用户是否有无单收货且未关闭的单据，如果有则用这个收货单（如果多个取最后一个）并新建收货单明细
 			List<ReceiveHeader> receiveHeaderList = receiveBiz.getReceiveListByNonOrder(AuthUtil.getUserId());
 			// 订单行号
 			String lineNo = "0";
 			int lineNum = 0;
 			ReceiveHeader receiveHeader = new ReceiveHeader();
-			if(Func.isNotEmpty(receiveHeaderList)){
+			if (Func.isNotEmpty(receiveHeaderList)) {
 				//取无单收货最后一条收货单
-				  receiveHeader = receiveHeaderList.get(receiveHeaderList.size()-1);
+				receiveHeader = receiveHeaderList.get(receiveHeaderList.size() - 1);
 				//获取当前收货单关联的最新一条明细的行号
 				lineNo = receiveBiz.getReceiveDetailLinNo(receiveHeader.getReceiveId());
-			}else{
+			} else {
 				//否则新建一个收货单
-				 receiveHeader = receiveFactory.createReceiveHeader(request);
+				receiveHeader = receiveFactory.createReceiveHeader(request);
 				receiveBiz.newReceiveHeader(receiveHeader);
 			}
-			if(Func.isNotEmpty(lineNo)){
+			if (Func.isNotEmpty(lineNo)) {
 				lineNum = Integer.parseInt(lineNo);
 			}
-			for(ReceiveDetailLpnItemDto item: request.getReceiveDetailLpnItemDtoList()){
-				lineNum+=10;
+			for (ReceiveDetailLpnItemDto item : request.getReceiveDetailLpnItemDtoList()) {
+				lineNum += 10;
 				//根据id获取lpn实体
 				ReceiveDetailLpn lpn = receiveBiz.getReceiveDetailLpnById(item.getReceiveDetailLpnId());
 				//创建收货单明细
-				ReceiveDetail receiveDetail = receiveFactory.createReceiveDetail(request,item,lpn,receiveHeader,lineNum);
+				ReceiveDetail receiveDetail = receiveFactory.createReceiveDetail(request, item, lpn, receiveHeader, lineNum);
 				receiveBiz.newReceiveDetail(receiveDetail);
 				//更新lpn表的明细id
 				lpn.setReceiveDetailId(receiveDetail.getReceiveDetailId());
@@ -82,18 +85,19 @@ public class InStockBizImpl implements InStockBiz {
 
 	@Override
 	@Transactional(propagation = Propagation.NESTED, rollbackFor = Exception.class)
-	public PdaByPieceReceiveResponse receiptByPiece(PdaByPieceReceiveRequest request) {
+	public PdaByPieceReceiveResponse receiptByPcs(PdaByPieceReceiveRequest request) {
 		// 判断业务参数，是否可以正常收货、超收
-		receiveBiz.canReceive(request.getReceiveDetailId(), request.getSurplusQty());
+		//receiveBiz.canReceive(request.getReceiveDetailId(), request.getSurplusQty());
+		// 生成清点记录
+		ReceiveLog receiveLog = receiveLogBiz.newReceiveLog(request);
 		// 调用库存函数
 
-		// 生成清点记录
 		// 更新收货单明细状态
 		receiveBiz.updateReceiveDetail(request.getReceiveDetailId(), request.getSurplusQty());
 		// 更新收货单状态
 		receiveBiz.updateReciveHeader(request.getReceiveDetailId());
 		// 记录业务日志
-		receiveBiz.log(request.getReceiveId(),request.getReceiveDetailId(),request.getSurplusQty(),request.getSkuLot1());
+		receiveBiz.log(request.getReceiveId(), request.getReceiveDetailId(), request.getSurplusQty(), request.getSkuLot1());
 		return null;
 	}
 }
