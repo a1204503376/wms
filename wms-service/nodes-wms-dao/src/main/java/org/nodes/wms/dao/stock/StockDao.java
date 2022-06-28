6d8eb7bf784adbd300f81d9ec5e3b7499ebdd64c
wangzy <NOdes2022>
2022-06-28 13:42:00 +0800

    feat：根据物品编码、物品名称、 库位编码获取库存信息

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/stock/StockDao.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/stock/StockDao.java
@@ -1,5 +1,7 @@
 package org.nodes.wms.dao.stock;
 
+import org.nodes.wms.dao.stock.entities.Stock;
+
 import java.util.List;
 import java.util.Map;
 
@@ -12,9 +14,9 @@ public interface StockDao {
 	 * 根据库位id查询库存数量
 	 *
 	 * @param locIdList: 库位id集合
-	 * @return Map<String,Object>
+	 * @return Map<String, Object>
 	 */
-	Map<String,Object> getStockQtyByLocIdList(List<Long> locIdList);
+	Map<String, Object> getStockQtyByLocIdList(List<Long> locIdList);
 
 	/**
 	 * 根据库位id获取库存物品总数
@@ -23,4 +25,12 @@ public interface StockDao {
 	 * @return Integer
 	 */
 	Integer getStockSkuCountByLocIdList(List<Long> locIdList);
+
+	/**
+	 * @param skuCode 物品编码
+	 * @param skuName 物品名称
+	 * @param locCode 库位编码
+	 * @return 库存信息
+	 */
+	Stock getStockBySku(String skuCode, String skuName, String locCode);
 }
