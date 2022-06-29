5c332f281979021d1888d832e676ca536e5d9938
pengyongcheng <pyc_1215@163.com>
2022-06-29 10:22:51 +0800

    feat: 新增库存占用和序列号日志Dao接口

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/stock/impl/SerialLogDaoImpl.java
@@ -0,0 +1,11 @@
+package org.nodes.wms.dao.stock.impl;
+
+import org.nodes.wms.dao.stock.SerialLogDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 序列号日志Dao接口实现类
+ **/
+@Repository
+public class SerialLogDaoImpl implements SerialLogDao {
+}
