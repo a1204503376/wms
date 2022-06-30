8cee02f91fe847df54b6ad348ad8f81e2add255b
wangzy <NOdes2022>
2022-06-30 18:26:53 +0800

    feat：按箱上架查询页面二数据的请求对象

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/instock/putaway/dto/input/PutawayByBoxRequest.java
@@ -0,0 +1,22 @@
+package org.nodes.wms.dao.instock.putaway.dto.input;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 按箱上架请求对象
+ **/
+@Data
+public class PutawayByBoxRequest implements Serializable {
+
+	private static final long serialVersionUID = 5278548007801352620L;
+	/**
+	 * 箱码
+	 */
+	private String boxCode;
+	/**
+	 * 库房id
+	 */
+	private Long whId;
+}
