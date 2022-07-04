7a0cbed9188a104e531a18159a7a63ba2912364d
邓茂林 <dengml@nodes.com.cn>
2022-07-04 18:20:07 +0800

    定义下发JOB接口

--- dispatching-service/scheduling/src/main/java/com/nodes/project/api/controller/WmsController.java
+++ dispatching-service/scheduling/src/main/java/com/nodes/project/api/controller/WmsController.java
@@ -1,7 +1,14 @@
 package com.nodes.project.api.controller;
 
+import com.nodes.project.api.dto.PublishJobRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 供WMS调用
@@ -9,13 +16,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dml
  */
 @RestController
-@RequestMapping("/wms")
+@Slf4j
+@RequestMapping("wms")
 public class WmsController {
 
-    // 下发JOB
-    // 取消JOB
-    // 终止JOB
+    /**
+     * 下发JOB
+     * WMS提供明细集合
+     */
+    @PostMapping("publishJob")
+    public void publishJob(@Validated @RequestBody List<PublishJobRequest> publishJobRequestList) {
+        log.info("下发JOB 请求参数：{}", publishJobRequestList);
+        
+    }
 
+    /**
+     * 取消JOB
+     */
+    @PostMapping("cancelJob")
+    public void cancelJob() {
+
+    }
+
+    /**
+     * 终止JOB
+     */
+    @PostMapping("terminationJob")
+    public void terminationJob() {
+
+    }
 
     // wms提供:
     // 回传状态的接口
@@ -23,9 +52,5 @@ public class WmsController {
     // 任务结束(正常)
     // 任务异常：返回AGV名称
 
-    // 本系统提供给AGV的接口：
-    // 订单开始
-    // 订单结束(正常)
-    // 订单异常：返回AGV名称
 
 }
