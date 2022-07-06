a4d567075ff980b3497164046a0a52e274051ca5
wangzy <NOdes2022>
2022-07-06 09:54:56 +0800

    feat：提交按箱收货biz接口定义

--- wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/putway/PutwayBiz.java
+++ wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/putway/PutwayBiz.java
@@ -1,6 +1,7 @@
 package org.nodes.wms.biz.putway;
 
 import org.nodes.wms.dao.basics.location.entities.Location;
+import org.nodes.wms.dao.putway.dto.input.NewPutawayByBoxlRequest;
 import org.nodes.wms.dao.stock.entities.Stock;
 
 import java.math.BigDecimal;
@@ -14,4 +15,10 @@ public interface PutwayBiz {
 	 * @return 可能为空或unkown库位
 	 */
 	Location runStrategyOfPutway(BigDecimal putwayQty, Stock sourceStock);
+
+	/**
+	 * 提交按箱收货
+	 * @param request 请求对象
+	 */
+	void  addByBoxShelf(NewPutawayByBoxlRequest request);
 }
--- wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/putway/impl/PutwayBizImpl.java
+++ wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/putway/impl/PutwayBizImpl.java
@@ -3,6 +3,7 @@ package org.nodes.wms.biz.putway.impl;
 import lombok.RequiredArgsConstructor;
 import org.nodes.wms.biz.putway.PutwayBiz;
 import org.nodes.wms.dao.basics.location.entities.Location;
+import org.nodes.wms.dao.putway.dto.input.NewPutawayByBoxlRequest;
 import org.nodes.wms.dao.stock.entities.Stock;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +15,10 @@ public class PutwayBizImpl implements PutwayBiz {
 	@Override
 	public Location runStrategyOfPutway(BigDecimal putwayQty, Stock sourceStock) {
 		return null;
+	}
+
+	@Override
+	public void addByBoxShelf(NewPutawayByBoxlRequest request) {
+
 	}
 }
