6799f811089d9909175bae33c981d8213432d1f4
wangzy <NOdes2022>
2022-07-06 16:51:50 +0800

    feat：参数管理新增或修该,把使用到的地方替换为最新版

--- wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/basics/systemParam/SystemParamBiz.java
+++ wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/basics/systemParam/SystemParamBiz.java
@@ -1,5 +1,7 @@
 package org.nodes.wms.biz.basics.systemParam;
 
+import org.nodes.core.base.entity.Param;
+
 import java.math.BigDecimal;
 
 public interface SystemParamBiz {
@@ -8,4 +10,26 @@ public interface SystemParamBiz {
 	 * @return
 	 */
 	BigDecimal findMaxLoadWeightOfColumn();
+
+	/**
+	 * 参数管理-新增或修改
+	 * @param param Param对象
+	 * @return 是否成功
+	 */
+	public Boolean saveOrUpdate(Param param);
+
+	/**
+	 * 根据参数的key获取参数
+	 *
+	 * @param key 键
+	 * @return Param
+	 */
+	public Param selectByKey(String key);
+
+	/**
+	 * 移除参数
+	 * @param ids 参数id集合
+	 * @return 是否成功
+	 */
+	public Boolean delete(String ids);
 }
