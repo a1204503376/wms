dd60c6fbb9ba5d9f372e9d374c886b07c972e2db
wangzy <NOdes2022>
2022-07-12 16:13:06 +0800

    feat：PDA批属性组件响应对象

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/basics/skulot/dto/output/FindAllSkuLotResponse.java
@@ -0,0 +1,15 @@
+package org.nodes.wms.dao.basics.skulot.dto.output;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * PDA批属性组件响应对象
+ **/
+@Data
+public class FindAllSkuLotResponse implements Serializable {
+	private static final long serialVersionUID = -2436813039015326126L;
+
+
+}
