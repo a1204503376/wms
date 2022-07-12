0513d918888e68fadf12d64d33b248e49f7465dd
Liumh <1062646441@qq.com>
2022-07-12 18:19:09 +0800

    feat：更新获取批属性开放个数的函数

--- wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/basics/systemParam/SystemParamBiz.java
+++ wms-service/nodes-wms-biz/src/main/java/org/nodes/wms/biz/basics/systemParam/SystemParamBiz.java
@@ -7,16 +7,25 @@ import java.math.BigDecimal;
 public interface SystemParamBiz {
 	/**
 	 * 获取列的最大载重
-	 * @return
+	 *
+	 * @return 列的最大载重
 	 */
 	BigDecimal findMaxLoadWeightOfColumn();
 
 	/**
+	 * 获取开放的批属性个数,如果没有配置则返回30
+	 *
+	 * @return 批属性开放个数
+	 */
+	int findSkuLotNumberOfOpen();
+
+	/**
 	 * 参数管理-新增或修改
+	 *
 	 * @param param Param对象
 	 * @return 是否成功
 	 */
-	public Boolean saveOrUpdate(Param param);
+	Boolean saveOrUpdate(Param param);
 
 	/**
 	 * 根据参数的key获取参数
@@ -24,12 +33,13 @@ public interface SystemParamBiz {
 	 * @param key 键
 	 * @return Param
 	 */
-	public Param selectByKey(String key);
+	Param selectByKey(String key);
 
 	/**
 	 * 移除参数
+	 *
 	 * @param ids 参数id集合
 	 * @return 是否成功
 	 */
-	public Boolean delete(String ids);
+	Boolean delete(String ids);
 }
