4b5ca7d52731f8f52819ecdc768555c48f6a6bde
pengyongcheng <pyc_1215@163.com>
2022-07-13 15:18:00 +0800

    feat: 发货记录日志列表查询和导出

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/logSoPick/dto/input/LogSoPickPageQuery.java
@@ -0,0 +1,60 @@
+package org.nodes.wms.dao.outstock.logSoPick.dto.input;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 拣货日志记录分页请求类
+ **/
+@Data
+public class LogSoPickPageQuery implements Serializable {
+
+	private static final long serialVersionUID = -4352510917899908388L;
+
+	/**
+	 * 发货单编码
+	 */
+	private String soBillNo;
+
+	/**
+	 * 单据类型
+	 */
+	private List<String> billTypeCdList;
+
+	/**
+	 * 物品id
+	 */
+	private List<Long> skuIdList;
+
+	/**
+	 * 箱号
+	 */
+	private String boxCode;
+
+	/**
+	 * LPN
+	 */
+	private String lpnCode;
+
+	/**
+	 * 序列号
+	 */
+	private String snCode;
+
+	/**
+	 * 拣货时间 - 开始
+	 */
+	private String createTimeBegin;
+
+	/**
+	 * 拣货时间 - 结束
+	 */
+	private String createTimeEnd;
+
+	/**
+	 * 拣货人
+	 */
+	private String createUser;
+}
--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/logSoPick/mapper/LogSoPickMapper.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/logSoPick/mapper/LogSoPickMapper.java
@@ -4,8 +4,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Param;
+import org.nodes.wms.dao.outstock.logSoPick.dto.input.LogSoPickPageQuery;
+import org.nodes.wms.dao.outstock.logSoPick.dto.output.LogSoPicExcelResponse;
 import org.nodes.wms.dao.outstock.logSoPick.dto.output.LogSoPickIndexResponse;
 import org.nodes.wms.dao.outstock.logSoPick.dto.output.LogSoPickForSoDetailResponse;
+import org.nodes.wms.dao.outstock.logSoPick.dto.output.LogSoPickPageResponse;
 import org.nodes.wms.dao.outstock.logSoPick.entities.LogSoPick;
 import org.springframework.stereotype.Repository;
 
@@ -32,4 +35,21 @@ public interface LogSoPickMapper extends BaseMapper<LogSoPick> {
 	 * @return Page<LogSoPickForSoDetailResponse> 出库单查看明细 拣货记录日志信息分页响应对象
 	 */
     Page<LogSoPickForSoDetailResponse> pageForSoDetailBySoBillId(IPage<?> page, @Param("soBillId") Long soBillId);
+
+	/**
+	 * 分页查询
+	 *
+	 * @param page:              分页参数
+	 * @param logSoPickPageQuery: 分页条件参数
+	 * @return Page<LogSoPickPageResponse> 拣货记录发分页查询响应对象
+	 */
+    Page<LogSoPickPageResponse> page(IPage<?> page, @Param("param") LogSoPickPageQuery logSoPickPageQuery);
+
+	/**
+	 * 根据Query条件查询发货日志记录
+	 *
+	 * @param logSoPickPageQuery: 查询条件
+	 * @return List<LogSoPicExcelResponse>
+	 */
+	List<LogSoPicExcelResponse> listByQuery(@Param("param") LogSoPickPageQuery logSoPickPageQuery);
 }
