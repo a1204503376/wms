c6791757012cc6533cc59585dc4fdc5c4a21c178
wangzy <NOdes2022>
2022-07-15 11:35:57 +0800

    feat：库区常量-AGV发货接驳区

---
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/basics/zone/constant/ZoneConstant.java
@@ -0,0 +1,11 @@
+package org.nodes.wms.dao.basics.zone.constant;
+
+/**
+ * 库区常量
+ */
+public class ZoneConstant {
+	/**
+	 * AGV发货接驳区
+	 */
+	public static final String AGV_SHIPMENTS_CONNECTION_AREA = "WH1-PICKTO";
+}
