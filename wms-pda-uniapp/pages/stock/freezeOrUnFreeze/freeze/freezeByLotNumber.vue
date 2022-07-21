d958df340951f482defe2b612e30a4fad17967cb
wangzy <NOdes2022>
2022-07-21 16:55:08 +0800

    feat：库存控制-按批次号冻结基础页面搭建

--- wms-pda-uniapp/pages/stock/freezeOrUnFreeze/freeze/freezeByLotNumber.vue
+++ wms-pda-uniapp/pages/stock/freezeOrUnFreeze/freeze/freezeByLotNumber.vue
@@ -1,18 +1,72 @@
 <template>
 	<view>
-		
+		<u-navbar leftIconColor="#fff" @leftClick="esc()" :fixed="false" :autoBack="false"
+			:bgColor="navigationBarBackgroundColor" title="按批次号冻结" titleStyle="color:#ffffff;font-size:21px"
+			style="color:#ffffff;font-size:21px">
+		</u-navbar>
+		<u--form>
+			<u-form-item label="批次号" :required="true" class="left-text-one-line" labelWidth="100">
+				<u--input v-model="params.lotNumber"></u--input>
+			</u-form-item>
+		</u--form>
+		<view class="footer">
+			<view class="btn-cancle" @click="esc()">
+				返回
+			</view>
+			<view class="btn-submit" @click="submit()">
+				确定
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import setting from '@/common/setting'
+	import receive from '@/api/inStock/receiveByPcs.js'
+	import uniSelect from '@/components/uni-select.vue'
+	import barcodeFunc from '@/common/barcodeFunc.js'
+	import tool from '@/utils/tool.js'
 	export default {
+		components: {
+			uniSelect
+		},
 		data() {
 			return {
-				
+				navigationBarBackgroundColor: setting.customNavigationBarBackgroundColor,
+				params: {
+					lotNumber: ''
+				}
 			}
 		},
+		onLoad() {
+		
+		},
+		onUnload() {
+			uni.$u.func.unRegisterScanner();
+		},
+		onShow() {
+			uni.$u.func.registerScanner(this.scannerCallback);
+		},
+		onBackPress(event) {
+			// #ifdef APP-PLUS
+			if (event.from === 'backbutton') {
+				this.esc();
+				return true;
+			}
+			// #endif
+		},
 		methods: {
-			
+			submit() {
+				var _this = this;
+				_this.params.isSn = true;
+				uni.$u.throttle(function() {
+					console.log('按批次号冻结成功')
+				}, 1000)
+
+			},
+			esc() {
+				uni.$u.func.navigateBackTo(1);
+			}
 		}
 	}
 </script>
