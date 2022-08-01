e1be8416f3907558bcfc1ca2388dea83e9da3d44
wangzy <NOdes2022>
2022-08-01 16:53:07 +0800

    feat：按箱码冻结，业务实现及前端实现

---
+++ wms-pda-uniapp/pages/stock/freezeOrUnFreeze/freeze/freezeByBoxCode.vue
@@ -0,0 +1,85 @@
+<template>
+	<view>
+		<u-navbar leftIconColor="#fff" @leftClick="esc()" :fixed="false" :autoBack="false"
+			:bgColor="navigationBarBackgroundColor" title="按箱码冻结" titleStyle="color:#ffffff;font-size:21px"
+			style="color:#ffffff;font-size:21px">
+		</u-navbar>
+		<u--form>
+			<u-form-item label="箱码" :required="true" class="left-text-one-line" labelWidth="100">
+				<u--input v-model="params.boxCode"></u--input>
+			</u-form-item>
+		</u--form>
+		<view class="footer">
+			<view class="btn-cancle" @click="esc()">
+				返回
+			</view>
+			<view class="btn-submit" @click="submit()">
+				确定
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import setting from '@/common/setting'
+	import receive from '@/api/inStock/receiveByPcs.js'
+	import uniSelect from '@/components/uni-select.vue'
+	import barcodeFunc from '@/common/barcodeFunc.js'
+	import tool from '@/utils/tool.js'
+	import freezeOrUnFreeze from '@/api/stock/freezeOrUnFreeze.js'
+	export default {
+		components: {
+			uniSelect
+		},
+		data() {
+			return {
+				navigationBarBackgroundColor: setting.customNavigationBarBackgroundColor,
+				params: {
+					boxCode: '',
+					whId:uni.getStorageSync('warehouse').whId
+				}
+			}
+		},
+		onLoad() {
+		
+		},
+		onUnload() {
+			uni.$u.func.unRegisterScanner();
+		},
+		onShow() {
+			uni.$u.func.registerScanner(this.scannerCallback);
+		},
+		onBackPress(event) {
+			// #ifdef APP-PLUS
+			if (event.from === 'backbutton') {
+				this.esc();
+				return true;
+			}
+			// #endif
+		},
+		methods: {
+			submit() {
+				var _this = this;
+				uni.$u.throttle(function() {
+					if (tool.isNotEmpty(_this.params.boxCode)) {
+						freezeOrUnFreeze.freezeByBoxCode(_this.params).then(data => {
+							console.log(data)
+						})
+						return;
+					}
+					_this.$u.func.showToast({
+						title: '请输入LOC'
+					});
+				}, 1000)
+
+			},
+			esc() {
+				uni.$u.func.navigateBackTo(1);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
