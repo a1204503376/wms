2dd0497ef64d221d9b22b07b31971e25c4e5928f
wangzy <NOdes2022>
2022-08-19 17:56:59 +0800

    feat：按件拣货 使用最新版本进行判断

--- wms-service/nodes-wms/src/main/java/org/nodes/wms/pdaController/picking/pickingByBox/PdaPickingByBoxController.java
+++ wms-service/nodes-wms/src/main/java/org/nodes/wms/pdaController/picking/pickingByBox/PdaPickingByBoxController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.nodes.core.constant.WmsApiPath;
 import org.nodes.wms.biz.outstock.OutStockBiz;
 import org.nodes.wms.dao.outstock.logSoPick.dto.input.PickByBoxCodeRequest;
+import org.nodes.wms.dao.task.enums.WmsTaskProcTypeEnum;
 import org.springblade.core.log.annotation.ApiLog;
 import org.springblade.core.tool.api.R;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +31,7 @@ public class PdaPickingByBoxController {
 	@ApiLog("PDA按箱拣货")
 	@PostMapping("/pickingByBox")
 	public R<String> pickingByBox(@RequestBody PickByBoxCodeRequest request) {
-		outStockBiz.pickByBox(request);
+		outStockBiz.pickByBox(request, WmsTaskProcTypeEnum.BY_BOX);
 		return R.success("PDA按箱拣货成功");
 	}
 }
