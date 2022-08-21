743df128d410b6c5a6608b69c5db1798cbb8585b
penghao <Nodes2022>
2022-08-21 20:25:38 +0800

    feat:解决冲突

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/SoPickPlanDao.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/SoPickPlanDao.java
@@ -8,6 +8,7 @@ import org.nodes.wms.dao.outstock.soPickPlan.dto.output.SoPickPlanPageResponse;
 import org.nodes.wms.dao.outstock.soPickPlan.entities.SoPickPlan;
 import org.springblade.core.mp.base.BaseService;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
