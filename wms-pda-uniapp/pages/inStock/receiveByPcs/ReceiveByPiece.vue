d7c24e2c409cb166533df1346a7831e9bb1d2fc9
wangzy <NOdes2022>
2022-08-24 09:32:32 +0800

    feat：跳转序列号在收货条件校验完成之后再进行跳转

--- wms-pda-uniapp/pages/inStock/receiveByPcs/ReceiveByPiece.vue
+++ wms-pda-uniapp/pages/inStock/receiveByPcs/ReceiveByPiece.vue
@@ -147,15 +147,15 @@
 					//提交表单数据 收货
 					paramsData.whCode = uni.getStorageSync('warehouse').whCode;
 					paramsData.whId = uni.getStorageSync('warehouse').whId;
-					if (_this.params.isSn) {
-						uni.$u.func.routeNavigateTo('/pages/inStock/receiveByPcs/collectionSerialNumber', _this
-							.params);
-						return;
-					}
 					if (tool.isNotEmpty(paramsData.skuLot2) && tool.isNotEmpty(paramsData.locCode) && tool
 						.isNotEmpty(paramsData.boxCode) && tool.isNotEmpty(_this.params.boxCode) && tool
 						.isNotEmpty(paramsData.skuLot1) && tool.isInteger(paramsData.surplusQty)) {
 						receive.submitReceiptByPcs(paramsData).then(data => {
+							if (_this.params.isSn) {
+								uni.$u.func.routeNavigateTo('/pages/inStock/receiveByPcs/collectionSerialNumber', _this
+									.params);
+								return;
+							}
 							if (data.data.allReceivieIsAccomplish && data.data
 								.currentReceivieIsAccomplish) {
 								//当前收货单收货收货完毕
