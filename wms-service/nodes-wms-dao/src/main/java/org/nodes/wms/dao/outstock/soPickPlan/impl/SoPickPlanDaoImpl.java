45f23b42668b35a0c4dd54bd46f68ce0939c0b5d
wangzy <NOdes2022>
2022-08-25 16:26:46 +0800

    feat：拣货计划数量 为空时直接赋值

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/soPickPlan/impl/SoPickPlanDaoImpl.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/soPickPlan/impl/SoPickPlanDaoImpl.java
@@ -13,6 +13,7 @@ import org.nodes.wms.dao.outstock.soPickPlan.entities.SoPickPlan;
 import org.nodes.wms.dao.outstock.soPickPlan.mapper.SoPickPlanMapper;
 import org.springblade.core.log.exception.ServiceException;
 import org.springblade.core.mp.base.BaseServiceImpl;
+import org.springblade.core.tool.utils.Func;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -25,8 +26,8 @@ import java.util.List;
  **/
 @Repository
 public class SoPickPlanDaoImpl
-		extends BaseServiceImpl<SoPickPlanMapper, SoPickPlan>
-		implements SoPickPlanDao {
+	extends BaseServiceImpl<SoPickPlanMapper, SoPickPlan>
+	implements SoPickPlanDao {
 
 	@Override
 	public List<SoPickPlanForDistributionResponse> getBySoBillIdAndSoDetailId(Long soBillId, Long soDetailId) {
@@ -37,25 +38,30 @@ public class SoPickPlanDaoImpl
 	public boolean hasEnablePickPlan(Long soBillId) {
 		AssertUtil.notNull(soBillId, "判断是否有分配中的计划失败，出库单ID为空");
 		return super.lambdaQuery()
-				.eq(SoPickPlan::getSoBillId, soBillId)
-				.count() > 0;
+			.eq(SoPickPlan::getSoBillId, soBillId)
+			.count() > 0;
 	}
 
 	@Override
 	public List<SoPickPlan> findBySoHeaderId(Long soBillId) {
 		AssertUtil.notNull(soBillId, "判断是否有分配中的计划失败，出库单ID为空");
 		return super.lambdaQuery()
-				.eq(SoPickPlan::getSoBillId, soBillId)
-				.list();
+			.eq(SoPickPlan::getSoBillId, soBillId)
+			.list();
 	}
 
 	@Override
 	public void updatePickRealQty(Long pickPlanId, BigDecimal pickRealQty) {
 		UpdateWrapper<SoPickPlan> updateWrapper = Wrappers.update();
 		updateWrapper.lambda()
-				.eq(SoPickPlan::getPickPlanId, pickPlanId);
+			.eq(SoPickPlan::getPickPlanId, pickPlanId);
 		SoPickPlan soPickPlan = new SoPickPlan();
-		soPickPlan.setPickRealQty(soPickPlan.getPickRealQty().add(pickRealQty));
+		if (Func.isEmpty(soPickPlan.getPickRealQty())) {
+			soPickPlan.setPickRealQty(pickRealQty);
+		} else {
+			soPickPlan.setPickRealQty(soPickPlan.getPickRealQty().add(pickRealQty));
+		}
+
 		if (!super.update(soPickPlan, updateWrapper)) {
 			throw new ServiceException("修改拣货计划失败,请再次重试");
 		}
@@ -65,8 +71,8 @@ public class SoPickPlanDaoImpl
 	public List<SoPickPlan> getPickByTaskId(Long taskId) {
 		AssertUtil.notNull(taskId, "根据任务查询关联的拣货计划失败，任务ID为空");
 		return super.lambdaQuery()
-				.eq(SoPickPlan::getTaskId, taskId)
-				.list();
+			.eq(SoPickPlan::getTaskId, taskId)
+			.list();
 	}
 
 	@Override
