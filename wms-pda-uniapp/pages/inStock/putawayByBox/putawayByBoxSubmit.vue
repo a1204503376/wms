bcf85184607a3c8a79400e3078fa32bd77159dcf
wangzy <NOdes2022>
2022-09-26 13:19:27 +0800

    feat:#16600；按箱上架不显示总数修复； bug产生原因数量没有赋值显示

--- wms-pda-uniapp/pages/inStock/putawayByBox/putawayByBoxSubmit.vue
+++ wms-pda-uniapp/pages/inStock/putawayByBox/putawayByBoxSubmit.vue
@@ -16,9 +16,9 @@
 			</u-form-item>
 			<u-form-item label="整托上架" :required="true" class="left-text-one-line" labelWidth="100">
 				<picker style="width: 100%;height: 100%;" v-model="dataSource" :range="isAllLpnPutawayList"
-					range-key="name" value="index" @change="bindPickerChange">
+					range-key="name" value="index" @change="bindPickerChange" >
 					<view class="uni-input-input" style="width: 100%;">
-						<u--input style="margin-top: 0rpx; z-index: 99999;" v-model.trim="dataSource"></u--input>
+						<u--input style="margin-top: 0rpx; z-index: 99999;" v-model.trim="dataSource" disabled></u--input>
 					</view>
 				</picker>
 			</u-form-item>
@@ -71,6 +71,7 @@
 		onLoad: function(option) {
 			var parse = JSON.parse(option.param)
 			this.params.boxCode = parse.boxCode;
+			this.params.qty = parse.qty;
 			this.dataSource = this.isAllLpnPutawayList[1].name;
 			this.params.isAllLpnPutaway = this.isAllLpnPutawayList[1].isAllLpnPutaway;
 		},
