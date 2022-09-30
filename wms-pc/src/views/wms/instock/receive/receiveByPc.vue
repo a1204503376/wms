e68be11e37f67a23d41899df2f40d134d5c9330f
pengyongcheng <pyc_1215@163.com>
2022-09-30 15:57:51 +0800

    fix: #16661; PC收货新增校验：相同箱码的物品得放在同一库位上

--- wms-pc/src/views/wms/instock/receive/receiveByPc.vue
+++ wms-pc/src/views/wms/instock/receive/receiveByPc.vue
@@ -243,7 +243,7 @@
                     <el-button
                         :loading="loading"
                         type="primary"
-                        @click="onSubmit">
+                        @click="onSave">
                         保 存
                     </el-button>
                     <el-button
@@ -470,8 +470,20 @@ export default {
                 skuLot2Exist: false
             }
         },
-        submitFormParams() {
+        onSave() {
             let detailRequestList = this.table.postData
+            for (const key in detailRequestList) {
+                let filterList = detailRequestList
+                    .filter(x => x.boxCode === detailRequestList[key].boxCode && x.locId !== detailRequestList[key].locId);
+                if (filterList.length > 0){
+                    this.$message.warning("相箱码同只能放在同一库位上");
+                    return
+                }
+            }
+            this.onSubmit();
+        },
+        submitFormParams() {
+            let detailRequestList = this.table.postData;
             let receiveByPcRequest = {receiveId: this.receiveId, detailRequestList}
             return ReceiveByPc(receiveByPcRequest)
                 .then(res => {
