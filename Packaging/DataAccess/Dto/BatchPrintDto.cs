10e5b3875a6274041b2dbc95179f8be91636c9b7
邓茂林 <dengml@nodes.com.cn>
2022-10-10 13:20:09 +0800

    1.修复web打印问题；2.修复序列号打印问题；

--- Packaging/DataAccess/Dto/BatchPrintDto.cs
+++ Packaging/DataAccess/Dto/BatchPrintDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Linq;
 using DataAccess.Enitiies;
@@ -35,7 +36,7 @@ namespace DataAccess.Dto
         public string PoCode { get; set; }
         public string WoCode { get; set; }
         public string SpecialCustomer { get; set; }
-        public List<SkuDetail> SkuDetails { get; set; }
+        public List<SkuDetailDto> SkuDetails { get; set; }
         public string Qty1 { get; set; }
         public string Qty2 { get; set; }
         public string Qty3 { get; set; }
@@ -46,7 +47,7 @@ namespace DataAccess.Dto
         {
             // 相关物品的数量进行合并后打印箱贴
             var skuDetails = SkuDetails.GroupBy(d => d.Sku)
-                .Select(g => new SkuDetail()
+                .Select(g => new SkuDetailDto()
                 {
                     Sku=g.Key,
                     PlanQty = g.Sum(p=>p.PlanQty)
@@ -56,7 +57,7 @@ namespace DataAccess.Dto
             for (var i = 0; i < skuDetails.Count; i++)
             {
                 var skuDetail = skuDetails[i];
-                var qty = $@"{(!oneFlag ? skuDetail.Sku.SkuName + " " : "")}{skuDetail.PlanQty:#########} {skuDetail.Sku.WspName}/箱";
+                var qty = $@"{(!oneFlag ? skuDetail.Sku.SkuNameS + " " : "")}{skuDetail.PlanQty:#########} {skuDetail.Sku.WspName}/箱";
                 switch (i)
                 {
                     case 0:
@@ -76,15 +77,31 @@ namespace DataAccess.Dto
         }
     }
 
-    public class SkuDetail
+    public class SkuDetailDto
     {
         public string SkuName { get; set; }
         public string SkuCode { get; set; }
         public string SkuSpec { get; set; }
-        public decimal PlanQty { get; set; }
         public string SkuLot1 { get; set; }
+        public decimal PlanQty { get; set; }
         public string TrackingNumber { get; set; }
         public Sku Sku { get; set; }
         public bool IsExcelVaildateOk { get; set; }
+    }
+
+    public class SkuDetailExportDto
+    {
+        [DisplayName("物品名称")]
+        public string SkuName { get; set; }
+        [DisplayName("物品编码")]
+        public string SkuCode { get; set; }
+        [DisplayName("型号")]
+        public string SkuSpec { get; set; }
+        [DisplayName("批次")]
+        public string SkuLot1 { get; set; }
+        [DisplayName("数量")]
+        public decimal PlanQty { get; set; }
+        [DisplayName("追踪号")]
+        public string TrackingNumber { get; set; }
     }
 }
