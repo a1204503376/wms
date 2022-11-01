62cc04a9a0f7301eafb0587a73a20a3defa0a1bd
penghao <Nodes2022>
2022-11-01 09:38:35 +0800

    fix: 修复sku组件默认值不显示问题

--- wms-pc/src/components/wms/select/NodesSku.vue
+++ wms-pc/src/components/wms/select/NodesSku.vue
@@ -57,128 +57,128 @@
 
 <script>
 
-    import {getSkuSelectByPage} from "@/api/wms/basics/sku";
-    import func from "@/util/func";
+import {getSkuSelectByPage} from "@/api/wms/basics/sku";
+import func from "@/util/func";
 
-    export default {
-        name: "NodesSkuQuery",
-        model: {
-            prop: 'selectVal',
-            event: 'selectValChange'
-        },
-        props: {
-            selectVal: [Object],
-            // 是否禁用 默认为 false不禁用
-            disabled: {type: Boolean, required: false, default: () => false},
-            // 组件大小，默认为mini, 支持 medium/small/mini
-            size: {type: String, required: false, default: () => "mini"},
-        },
-        data() {
-            return {
-                // 外层页面上显示的值
-                val: '',
-                columnList: [
-                    {
-                        label: "物品编码",
-                        prop: "skuCode",
-                        width: "125"
-                    },
-                    {
-                        label: "物品名称",
-                        prop: "skuName",
-                        width: "125"
-                    },
-                    {
-                        label: "物品规格",
-                        prop: "skuSpec",
-                        width: "125"
-                    }
-                ],
-                // 表格数据
-                tableData: [],
-                tableHeight: 229,
-                // 表格加载
-                tableLoading: false,
-                // 分页对象
-                page: {
-                    total: 0,
-                    size: 5,
-                    current: 1,
-                    ascs: "",
-                    descs: "",
+export default {
+    name: "NodesSkuQuery",
+    model: {
+        prop: 'selectVal',
+        event: 'selectValChange'
+    },
+    props: {
+        selectVal: [Object],
+        // 是否禁用 默认为 false不禁用
+        disabled: {type: Boolean, required: false, default: () => false},
+        // 组件大小，默认为mini, 支持 medium/small/mini
+        size: {type: String, required: false, default: () => "mini"},
+    },
+    data() {
+        return {
+            // 外层页面上显示的值
+            val: this.selectVal.skuCode,
+            columnList: [
+                {
+                    label: "物品编码",
+                    prop: "skuCode",
+                    width: "125"
                 },
-                options: [],
-                key: '',
-            }
-        },
-        watch: {
-            // 1、监听外部给的默认值。 或者 2、手动选择后，赋选中(下面回传的sku对象)的值。
-            selectVal(selectVal) {
-                if (func.isNotEmpty(selectVal.skuCode)) {
-                    this.val = selectVal.skuCode;
-                } else {
-                    this.key = undefined;
-                    this.val = undefined;
-                    this.tableData = [];
-                    this.page.total = undefined;
-                    this.options = [];
-                    this.$emit('selectValChange', undefined); // 回传sku对象
-                    this.$refs.select.blur(); //使组件去焦点，收起下拉选项
-                    return;
+                {
+                    label: "物品名称",
+                    prop: "skuName",
+                    width: "125"
+                },
+                {
+                    label: "物品规格",
+                    prop: "skuSpec",
+                    width: "125"
                 }
-            }
-        },
-        methods: {
-            onRowClick(row) {
-                this.$emit('selectValChange', row); // 回传sku对象
+            ],
+            // 表格数据
+            tableData: [],
+            tableHeight: 229,
+            // 表格加载
+            tableLoading: false,
+            // 分页对象
+            page: {
+                total: 0,
+                size: 5,
+                current: 1,
+                ascs: "",
+                descs: "",
+            },
+            options: [],
+            key: '',
+        }
+    },
+    watch: {
+        // 1、监听外部给的默认值。 或者 2、手动选择后，赋选中(下面回传的sku对象)的值。
+        selectVal(selectVal) {
+            if (func.isNotEmpty(selectVal.skuCode)) {
+                this.val = selectVal.skuCode;
+            } else {
+                this.key = undefined;
+                this.val = undefined;
+                this.tableData = [];
+                this.page.total = undefined;
+                this.options = [];
+                this.$emit('selectValChange', undefined); // 回传sku对象
                 this.$refs.select.blur(); //使组件去焦点，收起下拉选项
-            },
-            async remoteMethod(val) {
-                if (func.isNotEmpty(val)) {
-                    this.key = val
-                    await this.getTableData();
-
-                    // 根据物品编码搜索时，如果搜索结果只有一个且和当前关键字一致，则直接回传
-                    if (this.tableData.length === 1 && this.tableData[0].skuCode === val) {
-                        this.$emit('selectValChange', this.tableData[0]); // 回传sku对象
-                        this.$refs.select.blur(); //使组件去焦点，收起下拉选项
-                    }
-                }
-            },
-            async getTableData() {
-                if (func.isNotEmpty(this.key)) {
-                    let {data: {data}} = await getSkuSelectByPage(this.page, this.key)
-                    this.tableData = data.records;
-                    this.page.total = data.total;
-                    this.options = this.tableData;
-                }
-            },
-            handleCurrentChange(current) {
-                this.page.current = current;
-                this.getTableData();
-            },
-            // 下面两个方法暂时没用
-            handleSizeChange(size) {
-                this.page.size = size;
-                this.getTableData();
-            },
-            handleSortChange(column) {
-                let prop = column.prop;
-                let order = column.order;
-                if (order === "ascending") {
-                    this.page.ascs = prop;
-                    this.page.descs = "";
-                } else if (order === "descending") {
-                    this.page.descs = prop;
-                    this.page.ascs = "";
-                } else {
-                    this.page.ascs = "";
-                    this.page.descs = "";
-                }
-                this.getTableData();
-            },
+                return;
+            }
+        }
+    },
+    methods: {
+        onRowClick(row) {
+            this.$emit('selectValChange', row); // 回传sku对象
+            this.$refs.select.blur(); //使组件去焦点，收起下拉选项
         },
-    }
+        async remoteMethod(val) {
+            if (func.isNotEmpty(val)) {
+                this.key = val
+                await this.getTableData();
+
+                // 根据物品编码搜索时，如果搜索结果只有一个且和当前关键字一致，则直接回传
+                if (this.tableData.length === 1 && this.tableData[0].skuCode === val) {
+                    this.$emit('selectValChange', this.tableData[0]); // 回传sku对象
+                    this.$refs.select.blur(); //使组件去焦点，收起下拉选项
+                }
+            }
+        },
+        async getTableData() {
+            if (func.isNotEmpty(this.key)) {
+                let {data: {data}} = await getSkuSelectByPage(this.page, this.key)
+                this.tableData = data.records;
+                this.page.total = data.total;
+                this.options = this.tableData;
+            }
+        },
+        handleCurrentChange(current) {
+            this.page.current = current;
+            this.getTableData();
+        },
+        // 下面两个方法暂时没用
+        handleSizeChange(size) {
+            this.page.size = size;
+            this.getTableData();
+        },
+        handleSortChange(column) {
+            let prop = column.prop;
+            let order = column.order;
+            if (order === "ascending") {
+                this.page.ascs = prop;
+                this.page.descs = "";
+            } else if (order === "descending") {
+                this.page.descs = prop;
+                this.page.ascs = "";
+            } else {
+                this.page.ascs = "";
+                this.page.descs = "";
+            }
+            this.getTableData();
+        },
+    },
+}
 </script>
 <style>
 
