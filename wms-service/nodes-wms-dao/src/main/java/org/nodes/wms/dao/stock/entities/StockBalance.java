b614b5c25c21411a0ccc31817456e3805b179d15
penghao <Nodes2022>
2022-11-15 10:42:45 +0800

    feat: 新增库存结存实体

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/stock/entities/StockBalance.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/stock/entities/StockBalance.java
@@ -5,8 +5,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import org.nodes.wms.dao.basics.skulot.entities.SkuLotBaseEntity;
+import org.nodes.wms.dao.stock.enums.StockStatusEnum;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * 库存结存实体
@@ -14,6 +16,7 @@ import java.io.Serializable;
 @Data
 @TableName("stock_balance")
 public class StockBalance extends SkuLotBaseEntity implements Serializable {
+	private static final long serialVersionUID = 4781570401404803872L;
 	/**
 	 * 库存ID
 	 */
@@ -59,4 +62,52 @@ public class StockBalance extends SkuLotBaseEntity implements Serializable {
 	 * 结存日期
 	 */
 	private Data balanceDate;
+	/**
+	 * 入库数量
+	 */
+	private BigDecimal inStockQty;
+	/**
+	 * 出库数量
+	 */
+	private BigDecimal outStockQty;
+	/**
+	 * 期初数量
+	 */
+	private BigDecimal openingQty;
+	/**
+	 * 结存数量
+	 */
+	private BigDecimal balanceQty;
+	/**
+	 * 库存状态
+	 */
+	private StockStatusEnum stockStatus;
+	/**
+	 * 批次号
+	 */
+	private String lotNumber;
+	/**
+	 * 自定义字段1
+	 */
+	private String udf1;
+
+	/**
+	 * 自定义字段2
+	 */
+	private String udf2;
+
+	/**
+	 * 自定义字段3
+	 */
+	private String udf3;
+
+	/**
+	 * 自定义字段4
+	 */
+	private String udf4;
+
+	/**
+	 * 自定义字段5
+	 */
+	private String udf5;
 }
