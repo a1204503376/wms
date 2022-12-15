cc67760e30a88c04c2a63957bd00f26011c8c3fc
邓茂林 <dengml@nodes.com.cn>
2022-12-15 11:54:10 +0800

    修改纸箱的模板样式

--- Packaging/Packaging/Encasement/Reports/SerialPaperReport.cs
+++ Packaging/Packaging/Encasement/Reports/SerialPaperReport.cs
@@ -1,12 +1,9 @@
 ﻿using DataAccess.Dto;
 using DataAccess.Encasement;
 using DevExpress.XtraReports.UI;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
 using System.IO;
+using DevExpress.XtraPrinting.BarCode;
 
 namespace Packaging.Encasement.Reports
 {
@@ -17,12 +14,33 @@ namespace Packaging.Encasement.Reports
         private SerialPaperReport()
         {
             InitializeComponent();
+            // var qrCode = new XRBarCode
+            // {
+            //     Alignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter,
+            //     TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter,
+            //     AutoModule = true,
+            //     Dpi = 254F,
+            //     ShowText = false,
+            //     LocationFloat = new DevExpress.Utils.PointFloat(660.7466F, 25.00001F),
+            //     Padding = new DevExpress.XtraPrinting.PaddingInfo(10, 10, 0, 0, 96F),
+            //     Module = 5.08F,
+            //     SizeF = new System.Drawing.SizeF(314.2534F, 380F),
+            //     Symbology = new QRCodeGenerator
+            //     {
+            //         CompactionMode = QRCodeCompactionMode.Byte,
+            //         Version = QRCodeVersion.Version1
+            //     }
+            // };
+            // qrCode.StylePriority.UseTextAlignment = false;
+            // qrCode.ExpressionBindings.AddRange(new DevExpress.XtraReports.UI.ExpressionBinding[] {
+            //     new DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[BoxNumberLabel]")});
+            // this.xrTableCell6.Controls.Add(qrCode);
         }
 
         public SerialPaperReport(SerialNumberPrintDto serialNumberPrintDto)
             : this(new List<SerialNumberPrintDto> { serialNumberPrintDto })
         {
-
+            
         }
 
         public SerialPaperReport(List<SerialNumberPrintDto> serialNumberPrintDtoList) : this()
--- Packaging/PackagingWeb/PredefinedReports/SerialPaperReport.cs
+++ Packaging/PackagingWeb/PredefinedReports/SerialPaperReport.cs
@@ -1,10 +1,4 @@
-﻿using DevExpress.XtraReports.UI;
-using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Drawing;
-
-namespace PackagingWeb.PredefinedReports
+﻿namespace PackagingWeb.PredefinedReports
 {
     public partial class SerialPaperReport : DevExpress.XtraReports.UI.XtraReport
     {
