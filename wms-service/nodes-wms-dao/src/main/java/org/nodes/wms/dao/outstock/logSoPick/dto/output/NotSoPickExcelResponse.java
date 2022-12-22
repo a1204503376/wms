23b388e2aaec892f7562c5f65aa21d17f055df3e
pengyongcheng <pyc_1215@163.com>
2022-12-22 16:12:20 +0800

    fix: #17186; 未发货记录全量导出批属性名字应对应天宜场景

--- wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/logSoPick/dto/output/NotSoPickExcelResponse.java
+++ wms-service/nodes-wms-dao/src/main/java/org/nodes/wms/dao/outstock/logSoPick/dto/output/NotSoPickExcelResponse.java
@@ -3,7 +3,6 @@ package org.nodes.wms.dao.outstock.logSoPick.dto.output;
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
-import org.nodes.wms.dao.common.skuLot.BaseSkuLot;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -14,7 +13,7 @@ import java.util.Date;
  **/
 @Data
 @ColumnWidth(15)
-public class NotSoPickExcelResponse extends BaseSkuLot implements Serializable {
+public class NotSoPickExcelResponse implements Serializable {
 
 	private static final long serialVersionUID = -1629114343276457635L;
 
@@ -56,12 +55,6 @@ public class NotSoPickExcelResponse extends BaseSkuLot implements Serializable {
 	private String skuName;
 
 	/**
-	 * 计量单位
-	 */
-	@ExcelProperty("计量单位")
-	private String umName;
-
-	/**
 	 * 计划数量
 	 */
 	@ExcelProperty("计划数量")
@@ -78,6 +71,60 @@ public class NotSoPickExcelResponse extends BaseSkuLot implements Serializable {
 	 */
 	@ExcelProperty("剩余数量")
 	private BigDecimal surplusQty;
+
+	/**
+	 * 计量单位
+	 */
+	@ExcelProperty("计量单位")
+	private String umName;
+
+	/**
+	 * 生产批次
+	 */
+	@ExcelProperty("生产批次")
+	private String skuLot1;
+
+	/**
+	 * 规格型号
+	 */
+	@ExcelProperty("规格型号")
+	private String skuLot2;
+
+	/**
+	 * 发货日期
+	 */
+	@ExcelProperty("发货时间")
+	private String skuLot3;
+
+	/**
+	 * 专用客户
+	 */
+	@ExcelProperty("专用客户")
+	private String skuLot4;
+
+	/**
+	 * 钢背批次
+	 */
+	@ExcelProperty("钢背批次")
+	private String skuLot5;
+
+	/**
+	 * 摩擦快批次
+	 */
+	@ExcelProperty("摩擦块批次")
+	private String skuLot6;
+
+	/**
+	 * 产品标识代码
+	 */
+	@ExcelProperty("产品标识代码")
+	private String skuLot7;
+
+	/**
+	 * 适用速度登记
+	 */
+	@ExcelProperty("适用速度等级")
+	private String skuLot8;
 
 	/**
 	 * 创建人
